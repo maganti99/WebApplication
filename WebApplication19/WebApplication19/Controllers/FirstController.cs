c9227436acb47c5a99b1a734cdc35e877df027f0
Nagesh Maganti <nmaganti9@gmail.com>
2017-11-08 23:47:00 -0500

    First add of web application

---
+++ WebApplication19/WebApplication19/Controllers/FirstController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication19.Controllers
+{
+    public class FirstController : Controller
+    {
+        //
+        // GET: /First/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
